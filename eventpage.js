67932f62eba5d14f44a998a261ad381c892ac5e2
Austin Riba <austin@m51.io>
2017-07-08 18:11:11 -0700

    Initial commit

---
+++ eventpage.js
@@ -0,0 +1,33 @@
+var urls = [
+    'facebook.com',
+    'instagram.com',
+    'reddit.com'
+]
+
+function makePayment(){
+  chrome.storage.sync.get({pastPayments: []}, function(value){
+    var allPayments = value.pastPayments;
+    var now = new Date(Date.now()).toLocaleString();
+    allPayments.push([5, now]);
+    chrome.storage.sync.set({pastPayments: allPayments});
+  });
+}
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if(changeInfo.status === 'complete'){
+        for (var i = 0; i < urls.length; i++) {
+          if(tab.url.includes(urls[i])){
+            console.log('BOOO! YOU ARE ON ' + urls[i]);
+            chrome.storage.sync.get({viewCount: 0}, function(value){
+                var newCount = value.viewCount + 1;
+                chrome.storage.sync.set({viewCount: newCount});
+                if(newCount % 1 === 0){
+                  makePayment();
+                }
+            })
+            break;
+          }
+        }
+    }
+});
+
--- popup.js
+++ popup.js
@@ -1,23 +1,44 @@
-function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-    callback(url);
-  });
+function renderTotal(views) {
+  var dollars = views * 0.50;
+  document.getElementById('status').textContent = dollars.toFixed(2);
 }
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+function buildPaymentTable(){
+  chrome.storage.sync.get({pastPayments: []}, function(value){
+    var table = document.getElementById('payments')
+    var tableLength = 5;
+
+    var startInx = value.pastPayments.length - 1;
+    if (startInx === -1) {
+      startInx = 0
+    }
+    var endInx = value.pastPayments.length - tableLength - 1;
+    if (endInx < 0){
+      endInx = 0
+    }
+    var displayThese = [];
+    for (var i = startInx; i >= endInx; i--) {
+      displayThese.push(value.pastPayments[i])
+    }
+
+    for (var i = 0; i < displayThese.length - 1; i++) {
+      var row = table.insertRow(i + 1);
+      var cell0 = row.insertCell(0);
+      var cell1 = row.insertCell(1);
+      var cell2 = row.insertCell(2);
+      cell0.innerHTML = displayThese[i][1];
+      cell1.innerHTML = '$5.00';
+      cell2.innerHTML = 'SFSafeHouse';
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    renderStatus('Url of this page: ' + url);
-  });
+  var viewCount = 0;
+  chrome.storage.sync.get({viewCount: 0}, function(value){
+    viewCount = value.viewCount;
+    renderTotal(viewCount);
+    buildPaymentTable();
+  })
 });
+
