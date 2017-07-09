6e0fc026f988865f7b33282248647623b9a2b30d
ehj <eheinrichjosties@gmail.com>
2017-07-09 12:09:36 -0700

    simplify table creation

--- popup.js
+++ popup.js
@@ -7,20 +7,8 @@ function buildPaymentTable(){
   chrome.storage.sync.get({pastPayments: []}, function(value){
     var table = document.getElementById('payments')
     var tableLength = 5;
-
-    var startInx = value.pastPayments.length - 1;
-    if (startInx === -1) {
-      startInx = 0
-    }
-    var endInx = value.pastPayments.length - tableLength - 1;
-    if (endInx < 0){
-      endInx = 0
-    }
-    var displayThese = [];
-    for (var i = startInx; i >= endInx; i--) {
-      displayThese.push(value.pastPayments[i])
-    }
-
+    var displayThese = value.pastPayments;
+    displayThese = displayThese.reverse().slice(0, tableLength);
     for (var i = 0; i < displayThese.length - 1; i++) {
       var row = table.insertRow(i + 1);
       var cell0 = row.insertCell(0);
@@ -41,4 +29,3 @@ document.addEventListener('DOMContentLoaded', function() {
     buildPaymentTable();
   })
 });
-
